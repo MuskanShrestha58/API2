4a90b77ae77ee1901926007376e1847fbadd9fca
Muskan Shrestha <160772010+MuskanShrestha58@users.noreply.github.com>
2024-03-30 10:31:18 +0545

    add the basic api

---
+++ MyApiProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MyApiProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyApiProject.settings')
+
+application = get_asgi_application()
